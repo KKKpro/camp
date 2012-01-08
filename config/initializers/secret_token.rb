91f5b23cb2a66031824bcaaf9074e89fdad13925
KKK <nlivemx@gmail.com>
2012-01-08 14:20:12 +0200

    Plain app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dolas::Application.config.secret_token = 'd72367a28d4e7e77cb7fde5ac43190905f563ec376c6772f0dc08f25809316679701eedb6c04cefe92d3e0a6860f31a342cf071c055f9d40ea5cfa61b7b4e452'
